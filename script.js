b0419532a509a6bffa53374ed06272209841688a
AJosueBN <anthonyjosue.bn@gmail.com>
2023-04-18 15:18:18 +0000

    Added an another variable so that I can concatenate data together

--- script.js
+++ script.js
@@ -2,25 +2,44 @@
 var generateBtn = document.querySelector("#generate");
 
 
-// The code here helps to generate a password
+// The code here helps to generate a password on the HTML page
 function generatePassword() {
-var upperCase = ['A','B','C','D','E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X','Y','Z' ];
-var lowerCase = ['a','b','c','d','e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x','y','z' ];
+var optionalPasscode = ' '
+var uppercaseChars = ['A','B','C','D','E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X','Y','Z' ];
+var lowercaseChars = ['a','b','c','d','e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x','y','z' ];
 var specialChars = ['!','£','$','%','^','&','*','(' , ')' ,'_' ,'+' ,'|' ,'{' , '}',':','~','<', '>','|'];
-var numerics = ['0','1','2','3','4','5','6','7','8','9'];
+var integerChars = ['0','1','2','3','4','5','6','7','8','9'];
 
-var length = prompt("How many lengths would you like your password to be?")
+// Prompt Area to let user know the criteria they can choose to generate their own password
+var length = prompt("How much length do you want your password to be?  Note:Between 8-128 characters")
 console.log(length)
 if(length < 8 || length > 128 || isNaN(length)) {
-window.alert("Password NEEDS to be between 8 to 128 characters")
-window.confirm("Yes or No")  
+alert("Password NEEDS to be between 8 to 128 characters")
+return false
+}
 
-var = prompt("Would you like lowercase and uppercase characters in your password?")
-console.log(lowercase + UpperCase)
+if (confirm("Would you like uppercase characters within your password?")) {
+optionalPasscode = optionalPasscode.concat(uppercaseChars);
+}
+if (confirm("Would you like lowercase characters within your password?")) {
+lowercaseChars = 
+}
+if (confirm("Would you like special characters within your password?")) {
+specialChars = 
+}
+if (confirm("Would you like numbered characters within your password?")) {
+integerChars = 
+}
+
+
 
 
 }
-return
+
+for (var i = 0; i < length; i++) {
+  var randomNumber = Math.floor(Math.random())
+
+
 }
 
 
