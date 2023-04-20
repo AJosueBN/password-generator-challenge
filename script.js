c43996018254d68442559945aaf72909cfbe32c2
AJosueBN <anthonyjosue.bn@gmail.com>
2023-04-20 13:57:17 +0000

    Added math floor and math random,still need to get generator to randomize characters in generator

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-
-// The code here helps to generate a password on the HTML page
-function generatePassword() {
+// Set these variables on a global scope rather than locally
 var optionalPasscode = ' '
 var uppercaseChars = ['A','B','C','D','E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X','Y','Z' ];
 var lowercaseChars = ['a','b','c','d','e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x','y','z' ];
 var specialChars = ['!','£','$','%','^','&','*','(' , ')' ,'_' ,'+' ,'|' ,'{' , '}',':','~','<', '>','|'];
 var integerChars = ['0','1','2','3','4','5','6','7','8','9'];
 
-// Prompt Area to let user know the criteria they can choose to generate their own password
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+
+// This code/function here helps to generate a password on the HTML page
+function generatePassword() {
+
+
+// Prompt Area to let user know the criteria they can choose to generate their own password(This comes before password gets generated)
 var length = prompt("How much length do you want your password to be?  Note:Between 8-128 characters")
 console.log(length)
 if(length < 8 || length > 128 || isNaN(length)) {
@@ -30,16 +34,19 @@ optionalPasscode = optionalPasscode.concat(specialChars);
 if (confirm("Would you like numbered characters within your password?")) {
 optionalPasscode = optionalPasscode.concat(integerChars);
 }
-return true
+
+for (var i = 0; i < length; i++)  {
+  var chosenNumber = Math.floor(Math.random() * optionalPasscode.length );
+  var randomPassword = ' ';
+  randomPassword = randomPassword + optionalPasscode[chosenNumber];
+  }
+  return randomPassword;
+  }
 
 
-}
+// This part allows the code to allocate/generate random letters or numbers
 
-for (var i = 0; i < length; i++) {
-var chosenNumber = Math.floor(Math.random() * optionalPasscode.length )
-var generatedPassword
 
-}
 
 
 // Write password to the #password input
